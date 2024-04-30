78f38c3323eef19f7f058ed8778586cfc4fd7077
Bhargav <bhargavd39@gmail.com>
2024-05-01 01:48:43 +0530

    made changes in api call

--- Sources/WebCall/WebCall.swift
+++ Sources/WebCall/WebCall.swift
@@ -1,38 +1,53 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+
+
+
 import Foundation
 
 public enum NetworkError: Error {
     case badRequest
     case decodingError
+    case invalidUrl
+    case invalidResponse
+    case internalServerError
 }
 
 public class Webservice {
     
     public init() { }
     
-    public func fetch<T: Codable>(url: URL, resultType: T.Type, completion: @escaping (Result<T?, NetworkError>) -> Void)  {
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data, error == nil,
-                  (response as? HTTPURLResponse)?.statusCode == 200
-            else {
-                completion(.failure(.decodingError))
-                return
+   public func getApiData<T:Decodable>(urlString: String, resultType: T.Type, completion:@escaping(Result<T, NetworkError>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.invalidUrl))
+            return
+        }
+        var urlRequest = URLRequest(url: url)
+               
+       // send Request
+        URLSession.shared.dataTask(with: urlRequest) { (responseData, httpUrlResponse, error) in
+            if error != nil {
+                completion(.failure(NetworkError.invalidUrl))
             }
-            let decoder = JSONDecoder()
-            do {
-                let result = try decoder.decode(T.self, from: data)
-                completion(.success(result))
+            if(error == nil && responseData != nil && responseData?.count != 0) {
+                let decoder = JSONDecoder()
+                do {
+                    let result = try decoder.decode(T.self, from: responseData!)
+                    completion(.success(result))
+                }
+                catch let error {
+                    print(error)
+                    completion(.failure(NetworkError.decodingError))
+                }
+            } else if (responseData != nil && responseData?.count != 0) {
+                completion(.failure(NetworkError.internalServerError))
             }
-            catch let error {
-                print(error)
-                completion(.failure(NetworkError.decodingError))
+            else if (error as? URLError)?.code == .timedOut {
+                completion(.failure(NetworkError.internalServerError))
             }
             
         }.resume()
-        
     }
     
 }
